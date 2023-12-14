085b92369804c7eef5de00e721912746a5704492
doodeck <8515450+doodeck@users.noreply.github.com>
2023-12-14 19:14:15 +0000

    Transfer LM to a separate file

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,8 @@
-import vertexai
-from vertexai.language_models import CodeChatModel
+from language_model import test_lm
 
-vertexai.init(project="proven-cosine-402618", location="us-central1")
-chat_model = CodeChatModel.from_pretrained("codechat-bison-32k")
-parameters = {
-    "max_output_tokens": 1024,
-    "temperature": 0.2
-}
-chat = chat_model.start_chat()
-response = chat.send_message("""Write Fibonacci code in python3""", **parameters)
-print(f"Response from Model: {response.text}")
+
+def main():
+    test_lm()
+
+if __name__ == "__main__":
+    main()
